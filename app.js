906ed60c812d938ff44c589fa454956528662343
silavsale <silavsale@gmail.com>
2020-06-19 09:47:12 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const OneYearReview = require("./OneYearReview.json");
+
+const {TechILearnedThisYear, TechIWillLearneNextYear} = OneYearReview;
+
+console.log("technologyIlearnedThisYear",TechILearnedThisYear[1]);
+console.log('-------------------------------------------------');
+// console.log("technologyIlearnedThisYear",TechILearnedThisYear );
+
